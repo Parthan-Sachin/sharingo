239cca6bcd9239c3a54990df30e0ab38feec6b98
Parthan-Sachin <parthan.parth123@gmail.com>
2020-01-19 01:12:05 +0530

    Added sensory evaluation form

--- sharingo-app/src/app/app.module.ts
+++ sharingo-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
@@ -22,6 +23,8 @@ import { FooterComponent } from './footer/footer.component';
 import { FoodRecordsComponent } from './foodRecords/foodRecords.component';
 import { LoginComponent } from './LoginPage/login.component';
 import { OrdersComponent } from './orders/orders.component';
+import { SensoryEvaluationComponent } from './sensory-evaluation/sensory-evaluation-edit.component';
+
 
 
 
@@ -35,15 +38,19 @@ import { OrdersComponent } from './orders/orders.component';
     DonorComponent,
     FoodRecordsComponent,
     LoginComponent,
-    OrdersComponent
+    OrdersComponent,
+    SensoryEvaluationComponent
+    
 
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
     NgxDatatableModule,
     RouterModule.forRoot([
-      
+      {path:'sensory',component:SensoryEvaluationComponent},
       {path:'dashboard',component:AppComponent},
       {path:'',component:LoginComponent},
       {path:'donor/newdonorform',component: DonorComponent},
---
+++ sharingo-app/src/app/sensory-evaluation/sensory-evaluation-edit.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+    selector: 'app-sensory-edit',
+    templateUrl: './sensory-evaluation-edit.component.html',
+    styleUrls: ['./sensory-evaluation-edit.component.css']
+  })
+
+export class SensoryEvaluationComponent implements OnInit{
+
+    sensoryForm =new FormGroup({});
+    public range: number = 1;
+    public fitValue: any;   
+    public fit:any;
+    
+    constructor(private fb:FormBuilder){
+
+    }
+
+    ngOnInit(){
+        this.sensoryForm = this.fb.group({
+            recipeName:[''],
+            appearance:[''],
+            aroma:[''],
+            taste:[''],
+            temperature:'',
+            consumption:[''],
+            acceptability:['']
+                  
+          })
+
+          console.log(this.fit);
+          if(this.fit > 25){
+            this.fitValue = 'yes';
+            this.sensoryForm.controls['temperature'].setValue(this.fitValue);
+          } else {
+            this.fitValue = 'no';
+            this.sensoryForm.controls['temperature'].setValue(this.fitValue);
+          }
+
+          /* this.sensoryForm.get('temperature').valueChanges.subscribe(
+              result => {
+                  if(result>25){
+                    this.fitValue = 'yes';
+                    this.sensoryForm.controls['temperature'].setValue(this.fitValue);
+                  } else {
+                    this.fitValue = 'no';
+                    this.sensoryForm.controls['temperature'].setValue(this.fitValue);
+                  }
+                   
+
+              }
+          ) */
+    }
+
+    formatLabel(value: number) {      
+        return value + '°C';
+      }
+
+    add(){
+        console.log(this.sensoryForm.get('temperature').value);
+    }
+
+    
+
+
+
+}
